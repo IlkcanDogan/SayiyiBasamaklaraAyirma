ac425e4d6005bdf89c61e72c7b47b2429010811f
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-03-29 00:12:32 +0300

    Add files via upload

---
+++ SayiyiBasamaklaraAyirma/SayiyiBasamaklaraAyirma/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SayiyiBasamaklaraAyirma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(34, 45);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(124, 20);
+            this.txtSayi.TabIndex = 0;
+            this.txtSayi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSayi.TextChanged += new System.EventHandler(this.txtSayi_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 286);
+            this.Controls.Add(this.txtSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi;
+    }
+}
+
---
+++ SayiyiBasamaklaraAyirma/SayiyiBasamaklaraAyirma/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayiyiBasamaklaraAyirma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int uzunluk = 0;
+       
+
+        int nokta = 0;
+        private void txtSayi_TextChanged(object sender, EventArgs e)
+        {
+
+            string sayi = txtSayi.Text;
+            uzunluk = sayi.Length;
+
+            uzunluk = uzunluk - nokta;
+            
+            if (uzunluk % 3 == 0)
+            {                     
+               txtSayi.Text = sayi + ".";
+               txtSayi.Select(txtSayi.Text.Length, 0);
+               nokta++;
+                
+            }
+            
+        }
+    }
+}
